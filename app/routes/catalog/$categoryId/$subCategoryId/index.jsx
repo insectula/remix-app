6445a2979e8ddf030398998b95c754ee36c128a2
insectula <insectula.94@gmail.com>
2022-06-23 22:21:01 +0300

    23june, BOOTSTRAP disabled

--- app/routes/catalog/$categoryId/$subCategoryId/index.jsx
+++ app/routes/catalog/$categoryId/$subCategoryId/index.jsx
@@ -1,12 +1,128 @@
-import { useParams } from "@remix-run/react";
+import { Fragment } from "react";
+import { useLoaderData, useOutletContext } from "@remix-run/react";
+import { prisma } from '~/db';
+import ProductCard from "~/components/productCard";
 
-export default function () {
-    const params = useParams();
-    return (
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-        <h1>~/routes/catalog/{params.categoryId}/{params.subCategoryId}/index.jsx</h1>
-        <h2>ДЕФОЛТНЫЙ ТОВАР для {params.categoryId}/{params.subCategoryId}</h2>
-      </div>
-    );
+export const loader = async({params}) => {
+
+  
+  const data = {};
+  let subCategory = []
+  let subCat = {}
+  data.params = params
+  /*
+  СДЕЛАТЬ КАЖДЫЙ index.js ПО СТРУКТУРЕ 
+      {свой loader} + <компонент карточки /> + <прочие товары />
+          (логика отсеивания есть в книжке Порселлы)
+  ДОПИЛИТЬ ЛОГИКУ ОПРЕДЕЛЕНИЯ категория-ТОВАР / категория-ПОДКАТЕГОРИЯ
+  ДОБАВИТЬ ЛОГИКУ ВЫБОРА ТОВАРА ПО УМОЛЧАНИЮ
+  ДОБАВИТЬ ФИЛЬТРЫ
+  */
+ try { // Проверяем запрашивается ПОДкатегория или товар
+  subCategory = await prisma.catalog.findMany({
+    where: {
+      category: {equals: data.params.subCategoryId}
+    },
+    select: {
+      category: true
+    },
+  })
+  subCat = subCategory[0].category
+  } catch(e) {
+    console.log('33-LOADER ERROR:', e);  
+  } 
+  try { // Загружаем товары категории
+      const catalog = await prisma.catalog.findMany({
+        where: {
+          category: {equals: !(subCat && Object.keys(subCat).length === 0 && Object.getPrototypeOf(subCat) === Object.prototype)?data.params.subCategoryId:data.params.categoryId}
+        },
+        select: !(subCat && Object.keys(subCat).length === 0 && Object.getPrototypeOf(subCat) === Object.prototype)?{
+          subcat: {
+            select: {
+              name: true,
+              title: true,
+              picture: true,
+              content: true,
+              hidden: true,
+              filter: true
+            }
+          },
+        }:{
+          cat: {
+            select: {
+              name: true,
+              title: true,
+              picture: true,
+              content: true,
+              hidden: true,
+              filter: true
+            }
+          },
+        },
+      })
+      if (!(subCat && Object.keys(subCat).length === 0 && Object.getPrototypeOf(subCat) === Object.prototype)) {
+        const [first, ...others] = !(subCat && Object.keys(subCat).length === 0 && Object.getPrototypeOf(subCat) === Object.prototype)?catalog[0].subcat:catalog[0].cat;
+        data.product = first
+        data.products = others
+      }
+  } catch(e) {
+      console.log('62-LOADER ERROR:', e);
+  }
+  if (subCat && Object.keys(subCat).length === 0 && Object.getPrototypeOf(subCat) === Object.prototype) {
+    try {
+        const product = await prisma.catalog.findMany({
+          where: {
+            category: {equals: data.params.categoryId},
+          },
+          select: {
+            cat: {
+              where: {
+                name: {equals: data.params.subCategoryId}
+              },
+              select: {
+                name: true,
+                title: true,
+                picture: true,
+                content: true,
+                hidden: true,
+                filter: true
+              }
+            },
+          },
+        })
+        const products = await prisma.catalog.findMany({
+          where: {
+            category: {equals: data.params.categoryId},
+          },
+          select: {
+            cat: {
+              where: {
+                name: {not: data.params.subCategoryId}
+              },
+              select: {
+                name: true,
+                title: true,
+                picture: true,
+                content: true,
+                hidden: true,
+                filter: true
+              }
+            },
+          },
+        })
+        data.product = product[0].cat[0]
+        data.products = products[0].cat
+    } catch(e) {
+        console.log('85-LOADER ERROR:', e);
+    }
   }
   
+
+  return(data)
+}
+export default function () {
+  const data = useLoaderData();
+  return (
+    <ProductCard data={data} />
+  )
+}
--- app/routes/catalog/$categoryId/index.jsx
+++ app/routes/catalog/$categoryId/index.jsx
@@ -1,12 +1,42 @@
-import { useParams } from "@remix-run/react";
+
+import { useLoaderData, useOutletContext } from "@remix-run/react";
+import { prisma } from '~/db';
+import ProductCard from "~/components/productCard";
+
+export const loader = async({params}) => {
+  const data = {};
+    data.params = params
+    try {
+        const catalog = await prisma.catalog.findMany({
+          where: {
+            category: {equals: data.params.categoryId},
+          },
+          select: {
+            cat: {
+              select: {
+                name: true,
+                title: true,
+                picture: true,
+                content: true,
+                hidden: true,
+                filter: true
+              }
+            },
+          },
+        })
+        const [first, ...others] = catalog[0].cat;
+        data.product = first;
+        data.products = others;
+    } catch(e) {
+        console.log('LOADER ERROR:', e);
+    }
+
+    return(data)
+  }
 
 export default function () {
-    const params = useParams();
-    return (
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-        <h1>~/routes/catalog/{params.categoryId}/index.jsx</h1>
-        <h2>ДЕФОЛТНЫЙ ТОВАР для {params.categoryId}</h2>
-      </div>
-    );
-  }
-  
+  const data = useLoaderData();
+  return (
+    <ProductCard data={data} />
+  )
+}
